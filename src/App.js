c5588a106c67d99a7528b0e036e84b73b4d0c354
kjlee772 <kjlee50612@gmail.com>
2021-10-29 14:09:56 +0900

    error commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useState } from 'react/cjs/react.development';
 import './App.css';
 
 function App() {
+  const [file, set_file] = useState();
+  const [previewURL, set_previewURL] = useState();
+
+  const [object_name, set_object_name] = useState();
+
+  const handleFileOnChange = (event) => {
+    event.preventDefault();
+    let reader = new FileReader();
+    let file = event.target.files[0];
+    reader.onloadend = () => {
+      set_file(file);
+      set_previewURL(reader.result);
+    }
+    reader.readAsDataURL(file);
+  }
+
+  const send_image_options = {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+      'Accept': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    },
+    body: JSON.stringify({
+      name: file,
+      data: previewURL,
+    })
+  }
+  const option = {
+    url:'http://221.158.52.168:3003/send',
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+      'Accept': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    },
+    body: JSON.stringify({
+      name: file,
+      data: previewURL,
+    })
+  }
+  const send_image = () => {
+    console.log('send image');
+    // fetch('http://221.158.52.168:3003/send', send_image_options)
+    //   .then(res => res.json())
+    //   .then(res => {
+    //     console.log(res.message);
+    //   })
+    //   .catch(err => {
+    //     console.log('send image 문제');
+    //   })
+    axios(option).then(res=>res.json()).then(res => console.log(res));
+  }
+  const detect_image_options = {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+      'Accept': 'application/json',
+    },
+    body: JSON.stringify({
+      name: file,
+    }),
+  }
+  const detect_image = () => {
+    console.log('detect image');
+    fetch('http://221.158.52.168:3003/detect', detect_image_options)
+      .then(res => res.json())
+      .then(res => {
+        set_object_name(res.Res);
+      })
+      .catch(err => {
+        console.log('detect error');
+      })
+  }
+
+  let preview = null;
+  if (file != '') {
+    preview = <img src={previewURL} />
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input type='file' accept='image/jpg,image/png,image/jpeg,image/gif' name='profile_img' onChange={handleFileOnChange} />
+      </div>
+      {preview}
+      <button onClick={() => send_image()}>사진 전송</button>
+      <button onClick={() => detect_image()}>물체 인식</button>
     </div>
   );
 }
